fd91deef98a89130e59b9ba984013dcb6159ea7a
Momoh Sani Musa <sanim16@yahoo.com>
2025-06-24 23:50:14 +0100

    Day 002

--- code/_current_attempt/Day_001/tip_calculator.py
+++ code/_current_attempt/Day_001/tip_calculator.py
@@ -2,5 +2,5 @@ print("Welcome to the tip calculator")
 bill = float(input("What was the total bill? "))
 tip_percentage = int(input("How much tip would you like to give? 10, 12 or 15 "))
 number_of_people = int(input("How many people to split the bill? "))
-individual_amount = ((tip_percentage/100 + 1) * bill)/number_of_people
+individual_amount = round(((tip_percentage/100 + 1) * bill)/number_of_people, 2)
 print(f"Each person should pay: ${individual_amount}")
--- code/_current_attempt/log.md
+++ code/_current_attempt/log.md
@@ -16,6 +16,14 @@
 
 **Link to work:** https://github.com/Sanim16/100-days-of-code/tree/master/Day_001/
 
+### Day 2: Jun 24, 2025
+
+**Today's Progress**: Restarted the @udemy [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code).
+
+**Thoughts:** I did day 3 of the course.
+
+**Link to work:** https://github.com/Sanim16/100-days-of-code/tree/master/Day_002/
+
 
 <!--
 Sample Log Day Reporting Format
