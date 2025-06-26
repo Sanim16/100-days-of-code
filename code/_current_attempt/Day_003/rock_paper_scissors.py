4c9ece9b2d705329db28686d3f2c31948f234e9d
Momoh Sani Musa <sanim16@yahoo.com>
2025-06-26 04:58:10 +0100

    Day 003

--- code/_current_attempt/Day_003/rock_paper_scissors.py
+++ code/_current_attempt/Day_003/rock_paper_scissors.py
@@ -1,3 +1,54 @@
 import random
 
-print(random.randint(1, 10))
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+options = [rock, paper, scissors]
+computer_choice = random.randint(0,2)
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(options[player_choice])
+print("Computer chose:")
+print(options[computer_choice])
+if player_choice == computer_choice:
+    print("It's a draw")
+elif player_choice == 0:
+    if computer_choice == 1:
+        print("You lose")
+    else:
+        print("You win!")
+elif player_choice == 1:
+    if computer_choice == 0:
+        print("You win!")
+    else:
+        print("You lose")
+elif player_choice == 2:
+    if computer_choice == 0:
+        print("You lose")
+    else:
+        print("You win!")
+else:
+    print("Invalid choice, you lose")
