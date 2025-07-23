f5ca0142aa5684673809997602164f9e4de23565
Momoh Sani Musa <sanim16@yahoo.com>
2025-07-23 18:09:42 +0100

    Day 027

--- code/_current_attempt/Day_026/main.py
+++ code/_current_attempt/Day_026/main.py
@@ -1,41 +1,17 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
+#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {
     row.letter:row.code for (index, row) in df.iterrows()
 }
-# print(new_dict)
-
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_input = input("Enter the word to transcribe: ").upper()
-user_string = list(user_input)
 
 new_code_list = [
-    new_dict[char] for char in user_string
+    new_dict[char] for char in user_input
 ]
 print(new_code_list)
--- code/_current_attempt/log.md
+++ code/_current_attempt/log.md
@@ -216,6 +216,14 @@
 
 **Link to work:** https://github.com/Sanim16/100-days-of-code/tree/master/Day_026/
 
+### Day 27: Jul 19, 2025
+
+**Today's Progress**: Restarted the @udemy [100 Days of Code: The Complete Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code).
+
+**Thoughts:** I did day 27 of the course.
+
+**Link to work:** https://github.com/Sanim16/100-days-of-code/tree/master/Day_027/
+
 
 <!--
 Sample Log Day Reporting Format
